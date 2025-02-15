7768b9f618ea318e0c2ad762c9f5446ba288a842
Esimit Karlgusta <annoh.karlgusta@gmail.com>
2025-02-16 02:58:53 +0300

    Adding pending prescription view

--- views/pharmacy/pending_prescriptions.php
+++ views/pharmacy/pending_prescriptions.php
@@ -19,7 +19,30 @@ include '../../includes/sidebar.php';
 <div class="page-wrapper">
     <div class="page-body">
         <div class="container-xl">
+            <?php include '../../includes/alerts.php'; ?>
             
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Pending Prescriptions</h3>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-vcenter">
+                            <thead>
+                                <tr>
+                                    <th>Patient</th>
+                                    <th>Medication</th>
+                                    <th>Dosage</th>
+                                    <th>Prescribed By</th>
+                                    <th>Date</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody></tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
